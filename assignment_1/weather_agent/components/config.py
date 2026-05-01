36f2030e0a544f9af973cfe8b51d2c65e3cc3948
Daniel Blanco <danielblancocas1@gmail.com>
2026-05-01 17:14:06 +0200

    Fix fetch params in  API URL for location

--- assignment_1/weather_agent/components/config.py
+++ assignment_1/weather_agent/components/config.py
@@ -11,7 +11,7 @@ class Config(BaseSettings):
     """
     
     # API Endpoints
-    LOCATION_API_URL: str = "http://ip-api.com/json/"
+    LOCATION_API_URL: str = "http://ip-api.com/json/?fields=status,message,country,countryCode,region,regionName,city,lat,lon,timezone,offset"
     WEATHER_API_BASE_URL: str = "https://api.open-meteo.com/v1/forecast"
     
     # Request timeout settings
